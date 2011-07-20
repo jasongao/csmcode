fc0531420827ff09a94ad360b877c06d01067e60
anirudh <anirudh@hermes5.csail.mit.edu>
2011-07-20 18:01:47 -0400

    Made some changes to gpsFunctions.c t get it to be a region counter. Added logs from the 100 Node DSRC experiment with caching and 90 % reads in the synthetic benchmark

--- current/workspace/VNConsistent/src/edu/mit/csail/jasongao/vnconsistent/Globals.java
+++ current/workspace/VNConsistent/src/edu/mit/csail/jasongao/vnconsistent/Globals.java
@@ -1,8 +1,8 @@
 package edu.mit.csail.jasongao.vnconsistent;
 public class Globals {
-        final static public boolean CACHE_ENABLED_ON_START = true;
+        final static public boolean CACHE_ENABLED_ON_START = false;
         final static public double BENCHMARK_READ_DISTRIBUTION_ON_START = 0.9f;
-        final static public long BENCHMARK_START_DELAY = 100000L; // milliseconds
+        final static public long BENCHMARK_START_DELAY = 10000L; // milliseconds
  	      final static public String CSM_SERVER_NAME="128.30.66.123:5212";
         final static public int MINIMUM_LATITUDE=128898;
         final static public int MINIMUM_LONGITUDE=10384948;
